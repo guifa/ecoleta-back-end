fd0c2526d828433600efab5946a8dcbfa62a53a9
Luiz Guilherme Ferreira de Alcântara <luizgfa@gmail.com>
2020-06-05 19:06:21 -0300

    Add upload

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -15,8 +15,15 @@ class PointsController {
             .where('uf', String(uf))
             .distinct()
             .select('points.*');
+        
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.15.6:3333/uploads/${point.image}`
+            }
+        });
 
-        return response.json(points);
+        return response.json(serializedPoints);
     }
     
     async show(request: Request, response: Response) {
@@ -28,12 +35,17 @@ class PointsController {
             return response.status(404).json({message: 'Point not found'});
         }
 
+        const serializedPoint =  {
+            ...point,
+            image_url: `http://192.168.15.6:3333/uploads/${point.image}`
+        };
+
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return response.json({point, items});
+        return response.json({serializedPoint, items});
     }
 
     async create(request: Request, response: Response) {
@@ -50,7 +62,7 @@ class PointsController {
     
         const trx = await knex.transaction();
         const point = {
-            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -64,7 +76,9 @@ class PointsController {
     
         const point_id = insertedIds[0];
     
-        const pointItems = items.map((item_id: number) => {
+        const pointItems = items.split(',')
+                                .map((item: string) => Number(item.trim()))
+                                .map((item_id: number) => {
             return {
                 item_id,
                 point_id
